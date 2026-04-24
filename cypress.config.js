2975efe97a8abcccf862cf55476817bf48a29dbb
Antonio Carlos <acmsfilho@gmail.com>
2026-04-24 00:17:47 -0300

    configuração arquivo yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   projectId: '8fjp94',
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
+    video: true
   },
 });
